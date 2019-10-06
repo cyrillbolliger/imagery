735ba55d1e3af5ae70eda77e8a11833758872150
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-10-06 17:45:48 +0200

    test user observer

---
+++ tests/Unit/Observers/UserObserverTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+use RootSeeder;
+
+
+class UserObserverTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed(RootSeeder::class);
+    }
+
+    public function testCreating()
+    {
+        $manager = User::first();
+        $managed = factory(User::class)->make();
+
+        Auth::login($manager);
+
+        $managed->save();
+
+        $this->assertEquals($manager, $managed->addedBy);
+    }
+
+    public function testDeleting()
+    {
+        $user1 = factory(User::class)->create();
+        $email = $user1->email;
+
+        $user1->delete();
+        $this->assertEquals("del0000000001 $email", $user1->email);
+
+        $user2 = factory(User::class)->create(['email' => $email]);
+        $user2->delete();
+        $this->assertEquals("del0000000002 $email", $user2->email);
+    }
+
+    public function testRestoring()
+    {
+        $user  = factory(User::class)->create();
+        $email = $user->email;
+
+        $user->delete();
+        $this->assertEquals("del0000000001 $email", $user->email);
+
+        $user->restore();
+        $this->assertEquals($email, $user->email);
+    }
+}
