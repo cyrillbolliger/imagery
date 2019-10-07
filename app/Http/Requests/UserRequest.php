f0eab599edba45fb86862ddd1309ad43113fd9d4
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-10-07 19:19:12 +0200

    remove unused code

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -13,16 +13,6 @@ use Illuminate\Validation\Rule;
 class UserRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
