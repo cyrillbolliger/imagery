16c3a6165700c0f65e9f4b6c59a7dee7cf74c0b8
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-12-05 18:19:25 +0100

    whitelist files to deploy

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,10 @@
 
 DEPLOY_DIR="~/www/imagery.bolliger.tech"
 
-rsync -vrz --delete-after \
-  --exclude='.env' \
-  --exclude='deploy_rsa.enc' \
-  --exclude='node_modules' \
-  --exclude='storage' \
-  --exclude='tests' \
-  --exclude='vendor' \
-  $TRAVIS_BUILD_DIR/. $SSH_USER@$SSH_HOST:$DEPLOY_DIR
+mkdir deploy
+mv -t deploy app artisan bootstrap composer* config database public resources routes
 
-ssh $SSH_USER@$SSH_HOST "cd $DEPLOY_DIR && ~/bin/composer install --optimize-autoloader --no-dev && php artisan config:cache"
+rsync -vrz --delete-after \
+  $TRAVIS_BUILD_DIR/deploy $SSH_USER@$SSH_HOST:$DEPLOY_DIR
+
+ssh $SSH_USER@$SSH_HOST "cd $DEPLOY_DIR && ~bin/composer install --optimize-autoloader --no-dev && php artisan config:cache"
