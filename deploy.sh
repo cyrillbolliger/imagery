2b9df18d335216e4e50bbfe178de99bf29d34e9f
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-12-05 15:29:48 +0100

    add deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo 'success'
+DEPLOY_DIR="~/www/imagery.bolliger.tech"
+
+rsync -vrz --delete-after \
+  --exclude='.env' \
+  --exclude='deploy_rsa.enc' \
+  --exclude='node_modules' \
+  --exclude='storage' \
+  --exclude='tests' \
+  --exclude='vendor' \
+  $TRAVIS_BUILD_DIR/. $SSH_USER@$SSH_HOST:$DEPLOY_DIR
+
+ssh $SSH_USER@$SSH_HOST "cd $DEPLOY_DIR && composer install --optimize-autoloader --no-dev && php artisan config:cache"
