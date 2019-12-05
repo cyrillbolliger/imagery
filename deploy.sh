bd37c513e118c1f2d15dc950ae397e270636740e
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-12-05 16:23:50 +0100

    fix composer path

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ rsync -vrz --delete-after \
   --exclude='vendor' \
   $TRAVIS_BUILD_DIR/. $SSH_USER@$SSH_HOST:$DEPLOY_DIR
 
-ssh $SSH_USER@$SSH_HOST "cd $DEPLOY_DIR && composer install --optimize-autoloader --no-dev && php artisan config:cache"
+ssh $SSH_USER@$SSH_HOST "cd $DEPLOY_DIR && ~/bin/composer install --optimize-autoloader --no-dev && php artisan config:cache"
