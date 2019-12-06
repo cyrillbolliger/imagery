dbe091d67ccd17302ca938c5f80ef8ef45343bd0
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-12-06 09:06:56 +0100

    fix deploy path

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ mkdir deploy
 mv -t deploy app artisan bootstrap composer* config database public resources routes
 
 rsync -vrz --delete-after \
-  $TRAVIS_BUILD_DIR/deploy $SSH_USER@$SSH_HOST:$DEPLOY_DIR
+  $TRAVIS_BUILD_DIR/deploy/ $SSH_USER@$SSH_HOST:$DEPLOY_DIR
 
-ssh $SSH_USER@$SSH_HOST "cd $DEPLOY_DIR && $HOME/bin/composer install --optimize-autoloader --no-dev && php artisan config:cache"
+ssh $SSH_USER@$SSH_HOST "cd $DEPLOY_DIR && /~/bin/composer install --optimize-autoloader --no-dev && php artisan config:cache"
