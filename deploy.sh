fe69f923d1bf96a3577496e9dac6fbf2e530f539
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-12-06 15:24:28 +0100

    exclude .htaccess from deployment

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ DEPLOY_DIR="~/www/imagery.bolliger.tech/"
 mkdir deploy
 mv -t deploy app artisan bootstrap composer* config database public resources routes
 
-rsync -vrz --delete-after --exclude-file='.htaccess' \
+rsync -vrz --delete-after --exclude='.htaccess' \
   $TRAVIS_BUILD_DIR/deploy/ $SSH_USER@$SSH_HOST:$DEPLOY_DIR
 
 ssh $SSH_USER@$SSH_HOST "cd $DEPLOY_DIR && ~/bin/composer install --optimize-autoloader --no-dev && php artisan config:clear && php artisan config:cache"
