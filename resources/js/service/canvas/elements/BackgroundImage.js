4db5317555bf1e3e0cb63abeda0cb02f400f708e
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-12-07 21:08:09 +0100

    zoomable background image

--- resources/js/service/canvas/elements/BackgroundImage.js
+++ resources/js/service/canvas/elements/BackgroundImage.js
@@ -29,16 +29,41 @@ export default class extends Background {
     }
 
     _drawBackground() {
-        this._canvas.width = this._image.width;
-        this._canvas.height = this._image.height;
+        this._setCanvasSize();
+
+        const width = this._canvas.width;
+        const height = this._canvas.height;
 
         if (this._image) {
-            this._context.drawImage(this._image, 0, 0);
+            this._context.drawImage(this._image, 0, 0, width, height);
 
             // if we just draw the image, we dont get an error if the uploaded
             // given document isn't a processable image. if we, however repaint
             // it using the following line, an error is thrown as expected.
-            this._context.drawImage(this._canvas, 0, 0);
+            this._context.drawImage(this._canvas, 0, 0, width, height);
         }
+    }
+
+    _setCanvasSize() {
+        const aspectRatioImage = this._image.width / this._image.height;
+
+        let dW = this._image.width - this._containerWidth;
+        let dH = this._image.height - this._containerHeight;
+
+        dW = dW > 0 ? dW : 0;
+        dH = dH > 0 ? dH : 0;
+
+        let width, height;
+
+        if (dW < dH) {
+            width = this._containerWidth + this._zoom * dW;
+            height = width / aspectRatioImage;
+        } else {
+            height = this._containerHeight + this._zoom * dH;
+            width = height * aspectRatioImage;
+        }
+
+        this._canvas.width = width;
+        this._canvas.height = height;
     }
 }
