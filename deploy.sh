5b0e66f721f30fc82ebe1dbb9612eea0d2098ba4
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-12-09 20:27:00 +0100

    keep .env, storage and vendor files, clear caches

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,11 @@ DEPLOY_DIR="~/www/imagery.bolliger.tech/"
 mkdir deploy
 mv -t deploy app artisan bootstrap composer* config database public resources routes
 
-rsync -vrz --delete-after --exclude='.htaccess' \
+rsync -vrz --delete-after  \
+    --exclude='.htaccess' \
+    --exclude='.env' \
+    --exclude='storage' \
+    --exclude='vendor' \
   $TRAVIS_BUILD_DIR/deploy/ $SSH_USER@$SSH_HOST:$DEPLOY_DIR
 
-ssh $SSH_USER@$SSH_HOST "cd $DEPLOY_DIR && ~/bin/composer install --optimize-autoloader --no-dev && php artisan config:clear && php artisan config:cache"
+ssh $SSH_USER@$SSH_HOST "cd $DEPLOY_DIR && ~/bin/composer install --optimize-autoloader --no-dev && php artisan cache:clear && php artisan config:clear && php artisan config:cache"
