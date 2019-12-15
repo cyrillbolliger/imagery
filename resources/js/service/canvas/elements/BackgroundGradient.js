6c47a56d0dd71795f488197bf78f93f551794f8d
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-12-15 18:39:54 +0100

    center gradient

--- resources/js/service/canvas/elements/BackgroundGradient.js
+++ resources/js/service/canvas/elements/BackgroundGradient.js
@@ -8,8 +8,9 @@ export default class extends Background {
     _drawBackground() {
         const width = this._canvas.width;
         const height = this._canvas.height;
-        const shift = Math.tan(RotationAngle) * height;
-        const gradient = this._context.createLinearGradient(0, 0, shift, height);
+        const shiftX = Math.tan(RotationAngle) * height;
+        const shiftY = Math.tan(RotationAngle) * width;
+        const gradient = this._context.createLinearGradient(0, shiftY, shiftX, height);
 
         gradient.addColorStop(0, gradientColorDark);
         gradient.addColorStop(0.5, gradientColorLight);
