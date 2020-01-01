0d08feeb5963de134c07bd18a3179de37275187c
cyrillbolliger <cyrill.bolliger@gmail.com>
2020-01-01 15:52:01 +0100

    fix: remember token size

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,7 +25,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email'             => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token'    => Str::random(10),
+        'remember_token'    => Str::random(100),
         'added_by'          => User::first() ? User::first()->id : 1,
         'managed_by'        => Group::first() ? Group::first()->id : 1,
         'default_logo'      => null,
