c68489eddc3ab615d2647221f6d63763a4b80513
cyrillbolliger <cyrill.bolliger@gmail.com>
2020-02-07 13:52:43 +0100

    get network independent

--- tests/Unit/Http/Controllers/Upload/UploadStrategyTest.php
+++ tests/Unit/Http/Controllers/Upload/UploadStrategyTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use App\Http\Controllers\Upload\RegularUploadStrategy;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class UploadStrategyTest extends TestCase
@@ -27,8 +28,13 @@ class UploadStrategyTest extends TestCase
         $uploadDir     = trim(config('app.uploads_dir'), '/');
         $uploadDirPath = disk_path($uploadDir);
 
-        $imgOld = $this->faker->image($uploadDirPath, 50, 50, 'cats', true);
-        $imgNew = $this->faker->image($uploadDirPath, 50, 50, 'cats', true);
+        $imgOld = $uploadDirPath.DIRECTORY_SEPARATOR.Str::random().'-1.jpg';
+        $imgNew = $uploadDirPath.DIRECTORY_SEPARATOR.Str::random().'-2.jpg';
+
+        $srcImg = database_path('factories'.DIRECTORY_SEPARATOR.'Image.jpg');
+
+        copy($srcImg, $imgOld);
+        copy($srcImg, $imgNew);
 
         $ttl = config('app.uploads_ttl');
         touch($imgOld, time() - $ttl - 1);
