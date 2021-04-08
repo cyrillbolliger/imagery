a673ea5dc013d48e5df7da35c8500d0981065de2
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2021-04-09 00:23:14 +0200

    improved favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,6 +47,19 @@
     <link href="{{ mix('css/VueSearchSelect.css') }}" rel="stylesheet">
     <link href="{{ mix('css/vue2-animate.css') }}" rel="stylesheet">
 
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png?v=RyxBBgpl7J">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png?v=RyxBBgpl7J">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png?v=RyxBBgpl7J">
+    <link rel="manifest" href="/favicon/site.webmanifest?v=RyxBBgpl7J">
+    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg?v=RyxBBgpl7J" color="#84b414">
+    <link rel="shortcut icon" href="/favicon/favicon.ico?v=RyxBBgpl7J">
+    <meta name="apple-mobile-web-app-title" content="cd.gruene.ch">
+    <meta name="application-name" content="cd.gruene.ch">
+    <meta name="msapplication-TileColor" content="#84b414">
+    <meta name="msapplication-config" content="/favicon/browserconfig.xml?v=RyxBBgpl7J">
+    <meta name="theme-color" content="#ffffff">
+
 </head>
 <body>
 <div id="content" class="container-fluid">
