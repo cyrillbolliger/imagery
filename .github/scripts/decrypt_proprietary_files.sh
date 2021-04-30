478f8d7b1625fa917c6f49e091edca4f807bfaf0
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2021-04-30 12:44:36 +0200

    debug gh action

--- .github/scripts/decrypt_proprietary_files.sh
+++ .github/scripts/decrypt_proprietary_files.sh
@@ -1,15 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 set -euo pipefail
 
 cd $GITHUB_WORKSPACE
 
-echo "DEBUG: ${proprietary_key:0:2}****${proprietary_key:31}--"
+echo "DEBUG: ${PROPRIETARY_KEY:0:2}****${PROPRIETARY_KEY:31}--"
 
 openssl enc \
     -d -aes256 \
     -md sha512 \
     -pbkdf2 -iter 100000 \
-    -pass env:proprietary_key \
+    -pass env:PROPRIETARY_KEY \
     -in proprietary.tar.gz.enc \
     | tar -xzv
