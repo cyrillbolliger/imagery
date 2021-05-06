7a29bf30ebce69619646ab88c98d75d983ebfecb
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2021-05-06 23:45:16 +0200

    UX: add nice footer to emails

---
+++ resources/views/vendor/mail/html/message.blade.php
@@ -0,0 +1,32 @@
+@component('mail::layout')
+{{-- Header --}}
+@slot('header')
+@component('mail::header', ['url' => config('app.url')])
+{{ config('app.name') }}
+@endcomponent
+@endslot
+
+{{-- Body --}}
+{{ $slot }}
+
+{{-- Subcopy --}}
+@isset($subcopy)
+@slot('subcopy')
+@component('mail::subcopy')
+{{ $subcopy }}
+@endcomponent
+@endslot
+@endisset
+
+{{-- Footer --}}
+@slot('footer')
+@component('mail::footer')
+<img src="{{ asset('images/logo-dark-400.png') }}" alt="@lang('Logo')" width="200">
+
+&nbsp;
+
+{!! config('app.imprint_addr') !!}
+
+@endcomponent
+@endslot
+@endcomponent
---
+++ resources/views/vendor/mail/text/message.blade.php
@@ -0,0 +1,29 @@
+@component('mail::layout')
+    {{-- Header --}}
+    @slot('header')
+        @component('mail::header', ['url' => config('app.url')])
+            {{ config('app.name') }}
+        @endcomponent
+    @endslot
+
+    {{-- Body --}}
+    {{ $slot }}
+
+    {{-- Subcopy --}}
+    @isset($subcopy)
+        @slot('subcopy')
+            @component('mail::subcopy')
+                {{ $subcopy }}
+            @endcomponent
+        @endslot
+    @endisset
+
+    {{-- Footer --}}
+    @slot('footer')
+        @component('mail::footer')
+---
+
+{{ strip_tags(str_replace(['<br>','<br/>'], "\n", config('app.imprint_addr'))) }}
+        @endcomponent
+    @endslot
+@endcomponent
