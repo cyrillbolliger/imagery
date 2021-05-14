7559b7f2f8a7e1312ed7a4983e644e19bfd5ba60
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2021-05-15 00:56:30 +0200

    fix user observer on empty sub

--- tests/Unit/Observers/UserObserverTest.php
+++ tests/Unit/Observers/UserObserverTest.php
@@ -32,7 +32,7 @@ class UserObserverTest extends TestCase
         $this->assertEquals($manager, $managed->addedBy);
     }
 
-    public function testDeleting()
+    public function testDeleting_email()
     {
         $user1 = factory(User::class)->create();
         $email = $user1->email;
@@ -45,7 +45,28 @@ class UserObserverTest extends TestCase
         $this->assertEquals("del0000000002 $email", $user2->email);
     }
 
-    public function testRestoring()
+    public function testDeleting_sub_empty()
+    {
+        $user1 = factory(User::class)->create(['sub' => null]);
+
+        $user1->delete();
+        self::assertNull($user1->sub);
+    }
+
+    public function testDeleting_sub()
+    {
+        $user1 = factory(User::class)->create();
+        $sub = $user1->sub;
+
+        $user1->delete();
+        self::assertEquals("del0000000001 $sub", $user1->sub);
+
+        $user2 = factory(User::class)->create(['sub' => $sub]);
+        $user2->delete();
+        self::assertEquals("del0000000002 $sub", $user2->sub);
+    }
+
+    public function testRestoring_email()
     {
         $user  = factory(User::class)->create(['enabled' => true]);
         $email = $user->email;
@@ -55,5 +76,26 @@ class UserObserverTest extends TestCase
 
         $user->restore();
         $this->assertEquals($email, $user->email);
+    }
+
+    public function testRestoring_sub_empty()
+    {
+        $user  = factory(User::class)->create(['enabled' => true, 'sub' => null]);
+
+        $user->delete();
+        $user->restore();
+        self::assertNull($user->sub);
+    }
+
+    public function testRestoring_sub()
+    {
+        $user  = factory(User::class)->create(['enabled' => true]);
+        $sub = $user->sub;
+
+        $user->delete();
+        self::assertEquals("del0000000001 $sub", $user->sub);
+
+        $user->restore();
+        self::assertEquals($sub, $user->sub);
     }
 }
